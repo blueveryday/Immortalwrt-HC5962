3b8fb987198dd796563c9f5823643de9f977add9
blueveryday <29169337+blueveryday@users.noreply.github.com>
2020-11-29 19:02:13 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone "https://github.com/project-openwrt/openwrt.git" --branch "openwrt-18.06" --single-branch "openwrt"
+git clone "https://github.com/blueveryday/CTCGFW-Openwrt.git" --branch "openwrt-18.06" --single-branch "openwrt"
 pushd "openwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
