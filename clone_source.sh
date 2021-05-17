2aef68965bc653b78e3d5d0267e1114703a46e9e
blueveryday <29169337+blueveryday@users.noreply.github.com>
2021-05-17 20:40:39 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -4,9 +4,9 @@ git clone "https://github.com/immortalwrt/immortalwrt.git" --branch "openwrt-18.
 
 pushd "immortalwrt"; 
 
-sed -i 's/192.168.1.1/192.168.62.1/g' package/base-files/files/bin/config_generate                                            #修改路由器管理IP地址
-sed -i 's/ImmortalWrt/HC5962/g' package/base-files/files/bin/config_generate                                                  #修改路由器主机名
-sed -i -e 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio${devidx}.ssid=HC5962/g' -e 's/set wireless.default_radio${devidx}.encryption=none/set wireless.default_radio${devidx}.encryption=psk2/g' -e '/set wireless.default_radio${devidx}.encryption=psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i -e 's/192.168.1.1/192.168.62.1/g' -e 's/ImmortalWrt/HC5962/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
+sed -i -e 's/OpenWrt/HC5962/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改WiFi名称,添加加密方式和密码
 
 ./scripts/feeds update -a -f
 ./scripts/feeds install -a -f
