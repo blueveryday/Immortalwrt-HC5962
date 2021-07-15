9ed21be3020911666ac4de2e4978c843c9bf3527
blueveryday <29169337+blueveryday@users.noreply.github.com>
2021-07-15 08:16:27 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-git clone "https://github.com/immortalwrt/immortalwrt.git" --branch "openwrt-18.06" --single-branch "immortalwrt"
+git clone "https://github.com/coolsnowwolf/lede.git"
 
-pushd "immortalwrt"; 
+pushd "lede"; 
 
-sed -i -e 's/192.168.1.1/192.168.62.1/g' -e 's/ImmortalWrt/HC5962/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
+sed -i -e 's/192.168.1.1/192.168.62.1/g' -e 's/lede/HC5962/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
 sed -i -e 's/OpenWrt/HC5962/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 修改WiFi名称,添加加密方式和密码
+sed -i -e '/custom-feed/a\src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default       #增加ShadowSocksR Plus源
 
 ./scripts/feeds update -a -f
 ./scripts/feeds install -a -f
