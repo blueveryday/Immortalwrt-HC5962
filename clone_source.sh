0723f3eabb0214c240bb110ec8fb00d627e4b3b6
blueveryday <blueveryday@163.com>
2021-07-16 19:07:01 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -2,7 +2,7 @@
 
 git clone "https://github.com/coolsnowwolf/lede.git"
 
-pushd "immortalwrt"; 
+pushd "lede"; 
 
 sed -i -e 's/192.168.1.1/192.168.62.1/g' -e 's/lede/HC5962/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
 sed -i -e 's/OpenWrt/HC5962/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
