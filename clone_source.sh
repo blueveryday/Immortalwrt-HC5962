fec0d4717e796a10861fc69571f27755659990d5
blueveryday <blueveryday@163.com>
2022-11-08 12:52:24 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone "git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt.git"
+git clone "git clone https://github.com/immortalwrt/immortalwrt.git"
 
 pushd "immortalwrt"; 
 
